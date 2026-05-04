9a736ef63c827baaad82812cf60b8b9955d09d14
Vladimir <Vladimirfaton@gmail.com>
2026-05-04 01:45:41 +0100

    modification

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$dbname = "magasin";
-$user = "root";
-$pass = "";
+$host = "sql200.infinityfree.com";
+$dbname = "if0_41822709_XXX";
+$user = "if0_41822709";
+$pass = "Vladimir0929";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
